c2f9e61a6b8a1948d584fc5c9cecb21072006de5
tbongkim03 <tbongkim03@gmial.com>
2024-08-06 12:36:57 +0900

    fix

--- src/transform/api/util.py
+++ src/transform/api/util.py
@@ -4,12 +4,15 @@ def save2parqeut(ds_nodash, df):
     df.to_parquet(f'~/t2/test_parquet/ordered_parquet/', partition_cols=['year','month'])
 
 def merge(load_dt):
+
     m = str(int(load_dt[4:6]) - 1)
 
     if len(m) == 1:
         m = '0' + str(m)
 
     read_df = pd.read_parquet(f'~/t2/test_parquet/year={load_dt[0:4]}/month={m}')
+    read_df['year'] = str(load_dt[0:4])
+    read_df['month'] = str(load_dt[4:6])
     print(read_df.dtypes)
     cols = ['movieCd', 'movieNm', 'audiCnt', 'year', 'month']
     df = read_df[cols].copy()
